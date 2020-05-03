d41fbf08baf1fb3188e3f12d5ca24e876318d018
Alex20004044 <Alex20004044@yandex.ru>
2020-05-03 15:40:12 +0300

    w
    dd

---
+++ BydonInterpreter.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Hime.Redist;
+using Bydon;
+namespace TestHime
+{
+    public class BydonInterpreter
+    {
+        Dictionary<string, VariableType> globalVarTable = new Dictionary<string, VariableType>();
+
+        public void Interpret(ParseResult parseResult)
+        {
+            int result = Process(parseResult.Root);
+            Console.WriteLine(result);
+        }
+        int Process(ASTNode node)
+        {
+            if(node.Symbol.ID == BydonLexer.ID.TerminalNumber)
+            {
+                return Convert32To10(node.Value);
+            }
+            else if(node.Symbol.ID == BydonParser.ID.VariableVariableInit)
+            {
+                VariableType variableType;
+                variableType.typename = node.Children[0].Value;
+                variableType.value = Process(node.Children[2]);
+                string varName = node.Children[1].Value;
+
+                globalVarTable.Add(varName, variableType);
+
+                return variableType.value;
+            }
+
+            switch (node.Symbol.Name)
+            {
+                case "*":
+                    {
+                        return Process(node.Children[0]) * Process(node.Children[1]);
+                        break;
+                    }
+                case "/":
+                    {
+                        return Process(node.Children[0]) / Process(node.Children[1]);
+                        break;
+                    }
+                case "+":
+                    {
+                        if (node.Children.Count == 1)
+                        {
+                            return +Process(node.Children[0]);
+                        }
+                        else
+                        {
+                            return Process(node.Children[0]) + Process(node.Children[1]);
+                        }
+                    }
+                case "-":
+                    {
+                        if (node.Children.Count == 1)
+                        {
+                            return -Process(node.Children[0]);
+                        }
+                        else
+                        {
+                            return Process(node.Children[0]) - Process(node.Children[1]);
+                        }
+                        break;
+                    }
+                case "<":
+                    {
+                        if (Process(node.Children[0]) < Process(node.Children[1]))
+                        {
+                            return 1;
+                        }
+                        else
+                        {
+                            return 0;
+                        }
+                    }
+                case ">":
+                    {
+                        if (Process(node.Children[0]) > Process(node.Children[1]))
+                        {
+                            return 1;
+                        }
+                        else
+                        {
+                            return 0;
+                        }
+                    }
+                case "<=":
+                    {
+                        if (Process(node.Children[0]) <= Process(node.Children[1]))
+                        {
+                            return 1;
+                        }
+                        else
+                        {
+                            return 0;
+                        }
+                    }
+                case ">=":
+                    {
+                        if (Process(node.Children[0]) >= Process(node.Children[1]))
+                        {
+                            return 1;
+                        }
+                        else
+                        {
+                            return 0;
+                        }
+                    }
+                case "=":
+                    {
+                        if (Process(node.Children[0]) == Process(node.Children[1]))
+                        {
+                            return 1;
+                        }
+                        else
+                        {
+                            return 0;
+                        }
+                    }
+                case "<>":
+                    {
+                        if (Process(node.Children[0]) != Process(node.Children[1]))
+                        {
+                            return 1;
+                        }
+                        else
+                        {
+                            return 0;
+                        }
+                    }
+            }
+            Console.WriteLine("\n\nUnknown character");
+            Console.WriteLine(node.ToString() + " " + node.Symbol + " " + node.SymbolType + " " + node.Span + " " + node.Value + " " + node.Context.Content);
+            throw new Exception("Unknown character");
+            return int.MinValue;
+        }
+
+
+        public static int Convert32To10(string value)
+        {
+            int result = 0;
+            int add = 0;
+            int i;
+            if (value[0] == '-' || value[0]=='+')
+            {
+                i = 1;
+            }
+            else
+            {
+                i = 0;
+            }
+            for(; i < value.Length; i++)
+            {
+                if(value[i] >= '0' && value[i] <= '9')
+                {
+                    add = int.Parse(value[i].ToString());
+                }
+                else if(value[i] >= 'A' && value[i] <= 'V')
+                {
+                    add = value[i] - 'A' + 10;
+                }
+                else
+                {
+                    throw new Exception("Unknown character");
+                }
+                result = result * 32 + add;
+            }
+
+            if (value[0] == '-')
+            {
+                return -result;
+            }
+            else
+            {
+                return result;
+            }
+        }
+
+        public struct VariableType
+        {
+            public string typename;
+            public int value;
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,46 @@
+using System;
+using Hime.Redist; // the namespace for the Hime Runtime
+using Bydon; // default namespace for the parser is the grammar's name
+using System.IO;
+namespace TestHime
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            string path = @"D:\Bydon\Programs\ParserDebug_1.bd";
+            StreamReader streamReader = new StreamReader(path);
+            // Creates the lexer and parser
+            BydonLexer lexer = new BydonLexer(streamReader);
+            BydonParser parser = new BydonParser(lexer);
+            // Executes the parsing
+            ParseResult result = parser.Parse();
+            //streamReader.Close();
+            // Prints the produced syntax tree
+            Print(result.Root, new bool[] { });
+
+            BydonInterpreter bydonInterpreter = new BydonInterpreter();
+            bydonInterpreter.Interpret(result);
+
+                
+        }
+
+        private static void Print(ASTNode node, bool[] crossings)
+        {
+            for (int i = 0; i < crossings.Length - 1; i++)
+                Console.Write(crossings[i] ? "|   " : "    ");
+            if (crossings.Length > 0)
+                Console.Write("+-> ");
+            Console.WriteLine(node.ToString() + " " + node.Symbol + " " + node.SymbolType + " " + node.Span + " " + node.Value + " " + node.Context.Content);
+            for (int i = 0; i != node.Children.Count; i++)
+            {
+                bool[] childCrossings = new bool[crossings.Length + 1];
+                Array.Copy(crossings, childCrossings, crossings.Length);
+                childCrossings[childCrossings.Length - 1] = (i < node.Children.Count - 1);
+                Print(node.Children[i], childCrossings);
+            }
+        }
+        
+
+    }
+}
