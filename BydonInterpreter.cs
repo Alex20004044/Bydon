5b8a368538f93d4c220dfbe12ef72519123e7977
Alex20004044 <Alex20004044@yandex.ru>
2020-05-05 19:12:47 +0300

    .

--- BydonInterpreter.cs
+++ BydonInterpreter.cs
@@ -3,26 +3,39 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 using Hime.Redist;
 using Bydon;
 namespace TestHime
 {
     public class BydonInterpreter
     {
-        Dictionary<string, VariableType> globalVarTable = new Dictionary<string, VariableType>();
+        
+
+        Dictionary<string, ASTNode> gloabalFuncTable = new Dictionary<string, ASTNode>();
+        Stack<Dictionary<string, VariableType>> functionStack = new Stack<Dictionary<string, VariableType>>();
+        bool isReturn = false;
+
+        
+
         int errorCounter = 1;
-        public void Interpret(ParseResult parseResult)
+        TextWriter printOutput;
+        public void Interpret(ParseResult parseResult, TextWriter _printOutput = null)
         {
+            printOutput = _printOutput;
             int result = Process(parseResult.Root);
-            if(errorCounter == 1)
+            if (_printOutput == null)
             {
-                Console.WriteLine("Succesful compilation");
+                if (errorCounter == 1)
+                {
+                    Console.WriteLine("Succesful compilation");
+                }
+                else
+                {
+                    Console.WriteLine("Error compilation");
+                }
+                Console.WriteLine("Result " + result);
             }
-            else
-            {
-                Console.WriteLine("Error compilation");
-            }
-            Console.WriteLine("Result" + result);
         }
         int Process(ASTNode node)
         {
@@ -31,12 +44,10 @@ namespace TestHime
                 return Convert32To10(node.Value);
             }
 
-
-
             else if(node.Symbol.ID == BydonLexer.ID.TerminalVariable)
             {
                 VariableType variableType;
-                if(globalVarTable.TryGetValue(node.Value, out variableType))
+                if(functionStack.Peek().TryGetValue(node.Value, out variableType))
                 {
                     return variableType.value;
                 }
@@ -47,10 +58,24 @@ namespace TestHime
                     return int.MaxValue;
                 }
             }
+            else if (node.Symbol.ID == BydonLexer.ID.TerminalReturn)
+            {
+                VariableType variableType;
+                functionStack.Peek().TryGetValue("return", out variableType);
+                variableType.value = Process(node.Children[0]);
+                isReturn = true;
+                return int.MinValue;
+            }
+            else if(node.Symbol.ID == BydonLexer.ID.TerminalPrint)
+            {
+                PrintFunction(Process(node.Children[0]).ToString());
+                return int.MinValue;
+            }
+
             //Variable construction
             else if(node.Symbol.ID == BydonParser.ID.VariableVariableInit)
             {
-                VariableType variableType;
+                VariableType variableType = new VariableType();
                 int exp = Process(node.Children[node.Children.Count - 1]);
                 for (int i = 1; i < node.Children.Count - 1; i++)
                 {
@@ -58,40 +83,112 @@ namespace TestHime
                     variableType.value = exp;
                     string varName = node.Children[i].Value;
 
-                    if (globalVarTable.ContainsKey(varName))
+                    if (functionStack.Peek().ContainsKey(varName))
                     {
                         //Error
                         PrintError(varName + " variable already exists. Second initializtion is canceled. ");
                     }
                     else
                     {
-                        globalVarTable.Add(varName, variableType);
+                        functionStack.Peek().Add(varName, variableType);
                     }
                 }
                 return exp;
             }
             //Function process
-            else if(node.Symbol.ID == BydonParser.ID.VariableFunction)
+            else if (node.Symbol.ID == BydonParser.ID.VariableFunctionCall)
             {
-                Process(node.Children[0]);
+                ASTNode func;
+                if(gloabalFuncTable.TryGetValue(node.Children[0].Value, out func))
+                {
+                    Process(node.Children[1]);
+                    return Process(func);
+                }
+                else
+                {
+                    PrintError(node.Children[0].Value + " is undefined function");
+                }
+            }
+            //Create new stacc and add default variables
+            else if(node.Symbol.ID == BydonParser.ID.VariableFunctionCallArgs)
+            {
+                Dictionary<string, VariableType> varStack = new Dictionary<string, VariableType>();
+                for (int i = 0; i < node.Children.Count; i++)
+                {
+                    varStack.Add(i.ToString(), DefineVariableType(node.Children[i]));
+                }
+                functionStack.Push(varStack);
                 return int.MinValue;
             }
+            //Function define
+            else if(node.Symbol.ID == BydonParser.ID.VariableFunctionDefine)
+            {
+                VariableType returnVariable = new VariableType();
+                returnVariable.typename = node.Children[0].Value;
+                returnVariable.value = Process(node.Children[3]);
+                functionStack.Peek().Add("return", returnVariable);
+
+                Process(node.Children[2]);
+                Process(node.Children[4]);
+
+                return functionStack.Pop()["return"].value;
+            }
+            
+
+            else if(node.Symbol.ID == BydonParser.ID.VariableFunctionParametres)
+            {
+                Dictionary<string, VariableType> varTable = functionStack.Peek();
+                VariableType variableType;
+                VariableType stackVar;
+                for (int i = 0; i < node.Children.Count; i++)
+                {
+                    Process(node.Children[i]);
+                    varTable.TryGetValue(node.Children[i].Children[1].Value, out variableType);
+                    if(varTable.TryGetValue((i).ToString(), out stackVar))
+                    {
+                        variableType.value = stackVar.value;
+                        varTable.Remove((i).ToString());
+                    }
+
+                }
+                return int.MinValue;
+            }
+
+
             //Statement list process 
             else if (node.Symbol.ID == BydonParser.ID.VariableStatementList)
             {
                 for(int i = 0; i < node.Children.Count; i++)
                 {
-                    Process(node.Children[i]);
+                    int value = Process(node.Children[i]);
+                    if (isReturn)
+                    {
+                        isReturn = false;
+                        return value;
+                    }
                 }
                 return int.MinValue;
             }
-            //Statement process
-            else if(node.Symbol.ID == BydonParser.ID.VariableStatement)
-            {
+            //Attention
 
-                Process(node.Children[0]);
-                
-                return int.MinValue;
+            else if(node.Symbol.ID == BydonParser.ID.VariableProgram)
+            {
+                for(int i = 0; i < node.Children.Count; i++)
+                {
+                    InitFunc(node.Children[i]);
+                }
+                ASTNode funcNode;
+                if(gloabalFuncTable.TryGetValue("main", out funcNode))
+                {
+                    functionStack.Push(new Dictionary<string, VariableType>());
+                    int t =  Process(funcNode);
+                    return t;
+                }
+                else
+                {
+                    PrintError("Main function is not found");
+                    return int.MinValue;
+                }
             }
 
 
@@ -104,7 +201,16 @@ namespace TestHime
                     }
                 case "/":
                     {
-                        return Process(node.Children[0]) / Process(node.Children[1]);
+                        int num1 = Process(node.Children[0]);
+                        int num2 = Process(node.Children[1]);
+                        if(num2 == 0)
+                        {
+                            return int.MaxValue;
+                        }
+                        else
+                        {
+                            return num1 / num2;
+                        }
                         break;
                     }
                 case "+":
@@ -203,7 +309,7 @@ namespace TestHime
             return int.MinValue;
         }
 
-
+        
         public static int Convert32To10(string value)
         {
             int result = 0;
@@ -244,10 +350,49 @@ namespace TestHime
             }
         }
 
-        public struct VariableType
+        void InitFunc(ASTNode node)
+        {
+            string funcName = node.Children[1].Value;
+            if (gloabalFuncTable.ContainsKey(funcName))
+            {
+                PrintError(funcName + " function already exists. Second initializtion is canceled. ");
+            }
+            else
+            {
+                gloabalFuncTable.Add(funcName, node);
+            }
+        }
+        VariableType DefineVariableType(ASTNode node)
+        {
+            VariableType variable = new VariableType();
+            variable.typename = "big";
+            variable.value = Process(node);
+            return variable;
+        }
+
+        void PrintFunction(string exp)
+        {
+            if (printOutput == null)
+            {
+                Console.WriteLine("PRINT: " + exp);
+            }
+            else
+            {
+                printOutput.WriteLine(exp);
+            }
+        }
+
+        public class VariableType
         {
             public string typename;
             public int value;
+        }
+        public class FunctionType
+        {
+            public VariableType returnVariable;
+            public Dictionary<string, VariableType> localVarTab = new Dictionary<string, VariableType>();
+            public ASTNode functionNode;
+            public string funcName;
         }
         
         void PrintError(string message)
--- Program.cs
+++ Program.cs
@@ -6,23 +6,112 @@ namespace TestHime
 {
     class Program
     {
+        static public bool isAutoTest = true;
+        
+        static public int testCount = 3;
         public static void Main(string[] args)
         {
-          
-                string path = @"D:\Bydon\Programs\ParserDebug_1.bd";
-                StreamReader streamReader = new StreamReader(path);
-                // Creates the lexer and parser 
+            if(isAutoTest)
+            {
+                string basePath = @"D:\Bydon\Programs\ParserTest_";
+                for (int i = 0; i < testCount; i++)
+                {
+                    StreamReader streamReader_test = new StreamReader(basePath + i + "_IN" +".bd");
 
-                BydonLexer lexer = new BydonLexer(streamReader);
-                BydonParser parser = new BydonParser(lexer);
-                // Executes the parsing
-                ParseResult result = parser.Parse();
-                streamReader.Close();
-                // Prints the produced syntax tree
-                Print(result.Root, new bool[] { });
+                    BydonLexer lexer_test = new BydonLexer(streamReader_test);
+                    BydonParser parser_test = new BydonParser(lexer_test);
+                    ParseResult result_test = parser_test.Parse();
 
-                BydonInterpreter bydonInterpreter = new BydonInterpreter();
-                bydonInterpreter.Interpret(result);
+                    streamReader_test.Close();
+
+                    StreamWriter streamWriter = File.CreateText(basePath + i + "_OUT" + ".txt");
+
+                    BydonInterpreter bydonInterpreter_test = new BydonInterpreter();
+                    bydonInterpreter_test.Interpret(result_test, streamWriter);
+                    streamWriter.Close();
+                    Console.WriteLine("File " + i + " was interpretated");
+                }
+
+                int numPassedTest = 0;
+                for(int i = 0; i < testCount; i++)
+                {              
+                    
+                    StreamReader streamReader_test = new StreamReader(basePath + i + "_TEST" + ".txt");
+
+                    string str_out_name = basePath + i + "_OUT" + ".txt";
+                    StreamReader streamReader_out = new StreamReader(str_out_name);
+
+                    int cur_str_ind = 1;
+                    bool isAllCorrect = true;
+
+                    Console.WriteLine(i + "." + " ParserTest_" + i);
+                    Console.WriteLine("\t   out  test");
+
+                    while(!(streamReader_test.EndOfStream && streamReader_out.EndOfStream))
+                    {
+                        string str_out = streamReader_out.ReadLine();
+                        string str_test = streamReader_test.ReadLine();
+                        if (str_out != str_test)
+                        {
+                            if (isAllCorrect)
+                            {
+                                isAllCorrect = false;
+                            }
+                            Console.Write("\t" + cur_str_ind + ") " + str_out + " != " + str_test);
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine(" - Error");
+                            Console.ResetColor();
+                        }
+                        else
+                        {
+                            Console.Write("\t" + cur_str_ind + ") " + str_out + " == " + str_test);
+                            Console.ForegroundColor = ConsoleColor.Green;
+                            Console.WriteLine(" - Correct");
+                            Console.ResetColor();
+                        }
+                        cur_str_ind++;
+                    }
+                    if(isAllCorrect)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine("FILE Correct");
+                        Console.ResetColor();
+                        numPassedTest++;
+                    }
+                    else
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("FILE Error");
+                        Console.ResetColor();
+                        
+                    }
+                    Console.WriteLine("---------");
+
+                    streamReader_test.Close();
+                    streamReader_out.Close();
+                }
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("Test Results: " + numPassedTest + "/" + testCount);
+                Console.ResetColor();
+                Console.WriteLine("---------");
+            }
+
+            Console.WriteLine("\n\n\n");
+
+            string path = @"D:\Bydon\Programs\ParserDebug_1.bd";
+            StreamReader streamReader = new StreamReader(path);
+            // Creates the lexer and parser 
+
+            BydonLexer lexer = new BydonLexer(streamReader);
+            BydonParser parser = new BydonParser(lexer);
+            // Executes the parsing
+            ParseResult result = parser.Parse();
+            streamReader.Close();
+            // Prints the produced syntax tree
+            Print(result.Root, new bool[] { });
+
+            BydonInterpreter bydonInterpreter = new BydonInterpreter();
+            bydonInterpreter.Interpret(result);
         }
 
         private static void Print(ASTNode node, bool[] crossings)
